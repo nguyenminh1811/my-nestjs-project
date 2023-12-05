1c87685721a14f22c7ead7ec90dc2162edfbe07a
nguyenminh1811 <nguyenminh18112000@gmail.com>
2023-12-05 17:45:05 +0700

    feat(nest): init nestjs project

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.cjs'],
+  rules: {},
+};
