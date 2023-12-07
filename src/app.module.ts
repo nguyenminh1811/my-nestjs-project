9057c24b8ff9909d969a28fbf328bc2a1cd6565e
nguyenminh1811 <nguyenminh18112000@gmail.com>
2023-12-07 10:34:56 +0700

    feat(nest): update structure project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import serverConfig, { NodeEnv } from './config/types/server.config';
+import { v4 as uuid } from 'uuid';
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule.forFeature(serverConfig)],
+      useFactory: async (sConfig: ConfigType<typeof serverConfig>) => ({
+        pinoHttp: {
+          level: sConfig.nodeEnv != NodeEnv.PROD ? 'debug' : 'info',
+          redact: ['req.headers.authorization', 'req.headers.Authorization'],
+          formatters: {
+            level: (label) => ({ level: label }),
+          },
+          genReqId: function (req, res) {
+            const existingID = req.id ?? req.headers['x-request-id'];
+            if (existingID) return existingID;
+            const id = uuid();
+            res.setHeader('X-Request-Id', id);
+            return id;
+          },
+          serializers: {
+            req(req) {
+              req.body = req.raw.body;
+              return req;
+            },
+          },
+        },
+        exclude: [{ method: RequestMethod.ALL, path: 'health' }],
+      }),
+      inject: [serverConfig.KEY],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, PinoLogger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
+import serverConfig, {
+  NodeEnv,
+  ServerConfig,
+} from './config/types/server.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    exposedHeaders: ['Content-Disposition'],
+  });
+
+  app.useLogger(app.get(Logger));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  const sConfig: ServerConfig = app.get(serverConfig.KEY);
+
+  if (sConfig.nodeEnv !== NodeEnv.PROD) {
+    const config = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle('My application service')
+      .setDescription('My application service API')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  await app.listen(sConfig.port);
 }
 bootstrap();
+
+process.on('uncaughtExceptionMonitor', (err, origin) => {
+  const logger = new PinoLogger({ renameContext: 'main' });
+  logger.error({ error: err, origin }, `Caught exception: ${err}`);
+});
